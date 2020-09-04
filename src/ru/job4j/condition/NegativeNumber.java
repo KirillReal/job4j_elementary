af2bcb94033743b83bac97f44b6d9ced57d721c5
KirillReal <kiril5471710@gmail.com>
2020-09-05 00:33:42 +0300

    4.1. Операторы ветвлений [#348219]

---
+++ src/ru/job4j/condition/NegativeNumber.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class NegativeNumber {
+    public static void main(String[] args) {
+       negative(4);
+       negative(-4);
+    }
+
+    public static void negative(int number) {
+        if (number < 0) {
+            System.out.println("This is negative numbers");
+        }
+    }
+}
+
