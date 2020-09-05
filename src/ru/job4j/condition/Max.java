e2900996d26e8dd9c550dadc9ab52ccb5baed4c5
KirillReal <kiril5471710@gmail.com>
2020-09-06 01:52:45 +0300

    4.2. Тернарное сравнение [#348217]

---
+++ src/ru/job4j/condition/Max.java
@@ -0,0 +1,14 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        boolean yes = left >= right;
+        int result = yes ?  left : right;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int msg = Max.max(4, 5);
+        System.out.println(msg);
+    }
+}
