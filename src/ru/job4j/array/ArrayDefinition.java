8c70fede4fc154bdf3a49cc3cc5407228fefa5a5
KirillReal <kiril5471710@gmail.com>
2020-09-11 23:55:17 +0300

    6.0.3. Массивы и цикл for. [#348238]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,8 +5,14 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-        System.out.println("Размер массива short:" + ages.length);
-        System.out.println("Размер массива String:" + surnames.length);
-        System.out.println("Размер массива float:" + prices.length);
+        String[] names = new String[4];
+        names[0] = "Oliver Sykes";
+        names[1] = "Andy Black";
+        names[2] = "Ronnie Radke";
+        names[3] = "Austine Carlile";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
