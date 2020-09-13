c73230e2ed531ae1010fdae64b40479a1003c765
KirillReal <kiril5471710@gmail.com>
2020-09-14 00:04:07 +0300

    6.4.2. Слово заканчивается на ... [#348229]

---
+++ src/ru/job4j/array/EndsWith.java
@@ -0,0 +1,14 @@
+package ru.job4j.array;
+
+public class EndsWith {
+    public static boolean endsWith(char[] word, char[] post) {
+        boolean result = true;
+        for (int i = post.length - 1; i < 0; i--) {
+            if (word[i] != post[i]) {
+                result = false;
+            }
+        }
+
+        return result;
+    }
+}
