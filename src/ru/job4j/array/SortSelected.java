be50d38782ae79b481b845d733d5c192d93f6a2f
KirillReal <kiril5471710@gmail.com>
2020-09-17 00:47:59 +0300

    6.5.3. Сортировка выборкой. [#348231]

--- src/ru/job4j/array/SortSelected.java
+++ src/ru/job4j/array/SortSelected.java
@@ -1,17 +1,18 @@
 package ru.job4j.array;
 
 public class SortSelected {
-    @SuppressWarnings("checkstyle:LeftCurly")
     public static int[] sort(int[] data) {
-        for (int i = 0; i < data.length; i++) {    // i - номер текущего шага
-            int min = MinDiapason.findMin(data, 0, data.length);
-            int index = FindLoop.indexOf(data, min, 0, data.length);
+        for (int i = 0; i < data.length; i++) { // i - номер текущего шага
+            int min = data[i];
+            int index = i;
             for (int j = i + 1; j < data.length; j++) {
-                if (data[index] < data[i]) {
-                    int temp = min;
-                    data[i] = min;
+                if (data[j] < min) {
+                    index = j;
+                    min = data[j];
                 }
             }
+            data[index] = data[i];
+            data[i] = min;
         }
         return data;
     }
--- test/ru/job4j/array/SortSelectedTest.java
+++ test/ru/job4j/array/SortSelectedTest.java
@@ -12,4 +12,20 @@ public class SortSelectedTest {
         int[] expect = new int[] {1, 2, 3, 4, 5};
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenSort2() {
+        int[] input = new int[] {5, 8, 7, 9, 6};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {5, 6, 7, 8, 9};
+        assertThat(result, is(expect));
+    }
+    @SuppressWarnings("checkstyle:EmptyLineSeparator")
+    @Test
+    public void whenSort3() {
+        int[] input = new int[] {11, 12, 10};
+        int[] result = SortSelected.sort(input);
+        int[] expect = new int[] {10, 11, 12};
+        assertThat(result, is(expect));
+    }
 }
